9bcb3042bda3c4327276bdda82d7294b46897ba1
Francois Artis <francois.artis@epitech.eu>
2020-05-20 16:22:41 +0200

    [CLIENT][TWEET] Add TweetForm and TweetList

--- client/app/src/Home.js
+++ client/app/src/Home.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
+
+// import axios from 'axios';
 // import { Redirect } from "react-router-dom";
+
+// const baseURL = "http://localhost:4242";
+// axios.defaults.withCredentials = true;
+
+import TweetList from './TweetList'
+import TweetForm from './TweetForm'
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
-        let userLogin = localStorage.getItem('login');
-        console.log("userLogin from Home :", userLogin);
+
+        console.log("props", props.greeting);
+
+        console.log("this.props.search", this.props.search);
+        console.log("this.props.search", this.props.search);
+        // let userLogin = localStorage.getItem('login');
+        // console.log("userLogin from Home :", userLogin);
 
         this.state = {
-            loggedIn: userLogin,
+            loggedIn: "",
             redirection: false,
-            ici: "ici"
+            tweets: []
         }
-        if (!userLogin) {
-            userLogin = "";
-            this.setState({
-                redirection: true
-            })
-        }
+
+        const userLogin = localStorage.getItem('login');
+        console.log("+++userLogin from here", userLogin)
+        console.log("+++userLogin this state from here", this.state.loggedIn)
+
+        this.setState({
+            loggedIn: userLogin
+        })
+
+        console.log("+++userLogin this state from here", this.state.loggedIn)
+
+        // if (!userLogin) {
+        //     userLogin = "";
+        //     this.setState({
+        //         redirection: true
+        //     })
+        // }
 
         console.log("from Home state : ", this.state.loggedIn);
     }
 
+
+
     render() {
-        // const { redirection } = this.state;
-        // if (redirection) {
-        //     //Affichage de la redirection
-        //     return <Redirect to='/login' />;
-        // }
+
         return (
             <div>
                 <h1>Home</h1>
                 <h2>Welcome {this.state.loggedIn}</h2>
-                <h3>{this.state.ici}</h3>
-                <p>test</p>
+                <TweetForm />
+                <TweetList />
+
             </div>
         )
     }
