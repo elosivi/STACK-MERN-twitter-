9d532db0503c98690004b1ed2f51c451a4737d59
Francois Artis <francois.artis@epitech.eu>
2020-05-25 11:03:03 +0200

    [SERVER][TWEETS] Handling BLOCKED followers

--- client/app/src/Login.js
+++ client/app/src/Login.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import AuthContext from "./AuthContext"
-
 import axios from 'axios';
 import { Redirect } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
